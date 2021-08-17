f3eac3008d3e0e0c381f45858f88e121b375e7a4
phbord <phbord@gmail.com>
2021-08-17 09:26:20 +0200

    mqj

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from 'components/Header';
+import Home from 'components/home';
 
 function App() {
   return (
