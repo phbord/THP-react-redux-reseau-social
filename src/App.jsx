5c00f00dc728488aa93f960b2c40d32a4ac9773d
phbord <phbord@gmail.com>
2021-08-17 09:29:28 +0200

    mqj

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 import Header from 'components/Header';
 import Home from 'components/home';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <div className="container">
-          <Switch>
-            <Route path="/" component={Home} exact />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route path="/" component={Home} exact />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,6 @@
 import { createStore, combineReducers } from "redux";
-import pastasReducer from "redux/pastas/pastasReducer";
-import riceReducer from "redux/rice/riceReducer";
-import todolistReducer from "redux/todolist/todolistReducer";
 
 const rootReducer = combineReducers({
-  pastas: pastasReducer,
-  rice: riceReducer,
-  item: todolistReducer,
 })
 
 let store = createStore(
